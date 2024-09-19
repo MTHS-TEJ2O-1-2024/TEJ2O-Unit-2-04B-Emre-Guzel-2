e97b1a3f5018868dbfd25f328a25b9d9bc25a979
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-19 10:13:25 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Mr. Emre Gzuel
+ * Created on: Sep 19 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.showString('Emre Gzuel!')
