8824bbe7046e766c6c89c747b98fa691f9533dad
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-19 22:55:19 +0000

    I made temperature software app using python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,19 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Emre Guzel
+Created on: Sep 19 2024
+This module is a Micro:bit MicroPython program that measures the temperature.
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+
+
+while True:
+
+    if button_a.is_pressed():
+        
+        temperature = temperature()
+
+        display.scroll("The temperature is " + str(temperature)+'C')
+
